22abdaf0d6907386fe9e933e35ac5430f3534529
Timofey <timofei.chunarev@gmail.com>
2020-06-26 16:49:46 +0700

    ninth

--- src/pages/index/animation.js
+++ src/pages/index/animation.js
@@ -3,8 +3,13 @@ const annotationTextGeaderLine = document.querySelectorAll('.annotation-text__he
 const annotationTextConpany = document.querySelector('.annotation-text__text-conpany');
 const services = document.querySelector('.services');
 const threeActionsButton = document.querySelector('.three-actions-button');
-const footerActionsText = document.querySelector('.footer-actions__text')
-
+const footerActionsText = document.querySelector('.footer-actions__text');
+const actions = document.querySelector('.actions');
+const headerWrapper = document.querySelector('.header-wrapper');
+const footerWrapper = document.querySelector('.footer-wrapper');
+const businessDevelopmentTop = document.querySelector('.business-development-top');
+const businessDevelopmentCards = document.querySelector('.business-development-cards');
+ 
 annotationBackgroundImg[0].classList.add('annotation__background-img-active-opasity');
 
 setTimeout(function() {
@@ -51,26 +56,43 @@ function isVisible(clientHeight) {
     
     for(let i = 0; i < elements.length; i++) {
         
-        let elem = elements[i].getBoundingClientRect();        
-        console.log(elem.top)
-        console.log(clientHeight)
-        if (elem.top > 0 && elem.top < clientHeight) {
+        let elem = elements[i].getBoundingClientRect();
+
+        if (elem.bottom > 0 && elem.bottom < clientHeight) {
+
+
             switch (+(elements[i].dataset.anima)) {
                 case 1:
-                    elements[i].classList.add('FDP-pioner-active')
-            }
+                    elements[i].classList.add('is-active-position');
+                    break
+                case 2:
+                    actions.classList.add('action-is-active');
+                    setTimeout(function() {
+                        headerWrapper.classList.add('header-is-active');
+                        footerWrapper.classList.add('footer-is-active');
+                    }, 3000)
+                    break
+                case 3:
+                    businessDevelopmentTop.classList.add('bd-top-active');
+                    businessDevelopmentCards.classList.add('cards-active');
+                    break
+            }   
         }
     }
 }
 
-
+const body = document.querySelector('body');
+console.log('body: ', body);
 
 window.addEventListener('scroll', () => {
-    let clientHeight = document.documentElement.clientHeight;
+    let clientHeight = document.querySelector('body').clientHeight;
+    console.log('clientHeight: ', clientHeight);
 
     isVisible(clientHeight)
 })
 
+
+// Анимация, меняющая цвет текста по наведению на кнопку
 
 threeActionsButton.addEventListener('mouseover', function() {
     footerActionsText.classList.add('text-is-active');
@@ -79,3 +101,9 @@ threeActionsButton.addEventListener('mouseover', function() {
 threeActionsButton.addEventListener('mouseout', function() {
     footerActionsText.classList.remove('text-is-active');
 })
+
+
+// test
+
+
+
